fe14a2edd0859230ff15f090b534ea2c8c5befe3
Suryapag <suryapa9092@gmail.com>
2025-11-02 15:36:29 +0530

    laalu

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/health', (req,res)=> res.json({ok:true}));
-app.get('/api/hello', (req,res)=> res.json({msg:"hello from api"}));
+app.get('/api/hello', (req,res)=> res.json({msg:"Hi Laalu!"}));
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`Listening on ${port}`));
